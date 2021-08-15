b1432b6b01621006ac2e2f98cc7f07ee7952ff43
vyshak-sukumaran <vyshakvyshu791@gmail.com>
2021-08-15 17:03:49 +0530

    'newcommit'

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import login, register
+from .views import loginPage, register
 
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', loginPage, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,41 @@
+from django import forms
 from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 
 
 def register(request):
 
-    return render(request, 'accounts/register.html')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,"Account has been created for"+ user)
 
-def login(request):
+            return redirect('login')
+        
+
+
+    context = {'form':form}
+    return render(request, 'accounts/register.html',context)
+
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
 
     return render(request, 'accounts/login.html')
 
